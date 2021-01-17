935968b01eb9a810a573c3cd1ec306959c6c9986
Rohan Vachheta <38291892+rohanvachheta@users.noreply.github.com>
2021-01-17 18:09:29 +0530

    fix Scroll

--- src/assets/styles/globalStyles.js
+++ src/assets/styles/globalStyles.js
@@ -13,4 +13,27 @@ export default createGlobalStyle`
   p + p {
     ${tw`mt-3`};
   }
+
+  
+
+ ::-webkit-scrollbar-track
+{
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+	border-radius: 10px;
+	background-color: #F5F5F5;
+}
+
+  ::-webkit-scrollbar
+{
+	width: 4px;
+  background-color: #F5F5F5;
+  margin:12px;
+}
+
+ ::-webkit-scrollbar-thumb
+{
+	border-radius: 10px;
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+	background-color: #ed64a6;
+}
 `;
