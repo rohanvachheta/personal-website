1d129ea2d3e05725f1e2f54987850768ecb66d60
Rohan Vachheta <38291892+rohanvachheta@users.noreply.github.com>
2021-01-17 14:45:08 +0530

    fix :image

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
         background_color: `#ed64a6`,
         theme_color: `#ed64a6`,
         display: `minimal-ui`,
-        icon: `src/assets/images/gatsby-icon.png`
+        icon: `src/assets/images/rohan_image.jpg`
       }
     },
     `gatsby-plugin-offline`,
